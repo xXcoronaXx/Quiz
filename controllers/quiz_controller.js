ccf4262e4b06aa7695b7c7d632b0b4e14dab7604
xXcoronaXx <acevedo.corona@gmail.com>
2015-06-07 23:08:04 +0200

    fix id

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -10,14 +10,14 @@ exports.index = function (req, res) {
 
 // GET /quizes/:id
 exports.show = function (req, res) {
-	models.Quiz.find({where : { Id: req.params.quizId }}).then(function (quiz) {
+	models.Quiz.find({where : { id: req.params.quizId }}).then(function (quiz) {
 		res.render('quizes/show', { quiz: quiz });
 	})
 };
 
 // GET quizes/:id/answer
 exports.answer = function (req, res) {
-	models.Quiz.find({where : { Id: req.params.quizId }}).then(function (quiz) {
+	models.Quiz.find({where : { id: req.params.quizId }}).then(function (quiz) {
 		if (req.query.respuesta.toLowerCase() === quiz.respuesta) {
 			res.render('quizes/answer',{ quiz: quiz, respuesta: 'Correcto' });
 		} else {
