0e79c05938382b934fefaede9e6ec0ee743ada44
xXcoronaXx <acevedo.corona@gmail.com>
2015-06-11 16:16:56 +0200

    permisos de usuarios

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+// MW login required
+exports.loginRequired = function(req, res, next) {
+	if (req.session.user) {
+		next();
+	} else{
+		res.redirect('/login');
+	};
+};
+
 // GET /login
 exports.new = function(req, res) {
 	var errors = req.session.errors || {};
