ad6d3d7724305ff56fcb64189e91f3d7a902bc0a
xXcoronaXx <acevedo.corona@gmail.com>
2015-06-11 16:01:00 +0200

    login

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var routes = require('./routes/index');
 
 var partials = require('express-partials');
 var methodOverride = require('method-override');
+var session =  require('express-session');
 
 var app = express();
 
@@ -23,9 +24,21 @@ app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('Adivina2 2015'));
+app.use(session());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Helpers dinamicos
+app.use(function(req, res, next) {
+  // guardar el path para redireccionar
+  if (!req.path.match(/\/login|\/logout/)) {
+    req.session.redir = req.path;
+  } 
+
+  res.locals.session = req.session;
+  next();
+});
 
 app.use('/', routes);
 
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,6 +11,10 @@ router.get('/', function(req, res, next) {
 });
 
 router.param('quizId', quizController.load);
+
+router.get('/login', sessionController.new);
+router.post('/login', sessionController.create);
+router.get('/logout', sessionController.destroy);
 
 router.get('/author', function(req, res, next) { res.render('author',{ errors: []}); });
 
